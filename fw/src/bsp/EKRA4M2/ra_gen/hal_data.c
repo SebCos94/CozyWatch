a11d5501dfa7192d32b624ee5dbc9de7d94e296d
Sebastiano Costanzo <sebastiano.costanzo@danfoss.com>
2025-09-29 16:58:47 +0200

    added clock

--- fw/src/bsp/EKRA4M2/ra_gen/hal_data.c
+++ fw/src/bsp/EKRA4M2/ra_gen/hal_data.c
@@ -4,17 +4,19 @@ dtc_instance_ctrl_t g_transfer1_ctrl;
 
 #if (1 == 1)
 transfer_info_t g_transfer1_info DTC_TRANSFER_INFO_ALIGNMENT =
-{ .transfer_settings_word_b.dest_addr_mode = TRANSFER_ADDR_MODE_INCREMENTED,
-  .transfer_settings_word_b.repeat_area = TRANSFER_REPEAT_AREA_DESTINATION,
-  .transfer_settings_word_b.irq = TRANSFER_IRQ_END,
-  .transfer_settings_word_b.chain_mode = TRANSFER_CHAIN_MODE_DISABLED,
-  .transfer_settings_word_b.src_addr_mode = TRANSFER_ADDR_MODE_FIXED,
-  .transfer_settings_word_b.size = TRANSFER_SIZE_1_BYTE,
-  .transfer_settings_word_b.mode = TRANSFER_MODE_NORMAL,
-  .p_dest = (void*) NULL,
-  .p_src = (void const*) NULL,
-  .num_blocks = (uint16_t) 0,
-  .length = (uint16_t) 0, };
+		{ .transfer_settings_word_b.dest_addr_mode =
+				TRANSFER_ADDR_MODE_INCREMENTED,
+				.transfer_settings_word_b.repeat_area =
+						TRANSFER_REPEAT_AREA_DESTINATION,
+				.transfer_settings_word_b.irq = TRANSFER_IRQ_END,
+				.transfer_settings_word_b.chain_mode =
+						TRANSFER_CHAIN_MODE_DISABLED,
+				.transfer_settings_word_b.src_addr_mode =
+						TRANSFER_ADDR_MODE_FIXED,
+				.transfer_settings_word_b.size = TRANSFER_SIZE_1_BYTE,
+				.transfer_settings_word_b.mode = TRANSFER_MODE_NORMAL, .p_dest =
+						(void*) NULL, .p_src = (void const*) NULL, .num_blocks =
+						(uint16_t) 0, .length = (uint16_t) 0, };
 
 #elif (1 > 1)
 /* User is responsible to initialize the array. */
@@ -23,38 +25,38 @@ transfer_info_t g_transfer1_info[1] DTC_TRANSFER_INFO_ALIGNMENT;
 /* User must call api::reconfigure before enable DTC transfer. */
 #endif
 
-const dtc_extended_cfg_t g_transfer1_cfg_extend =
-{ .activation_source = VECTOR_NUMBER_SCI0_RXI, };
+const dtc_extended_cfg_t g_transfer1_cfg_extend = { .activation_source =
+		VECTOR_NUMBER_SCI0_RXI, };
 
-const transfer_cfg_t g_transfer1_cfg =
-{
+const transfer_cfg_t g_transfer1_cfg = {
 #if (1 == 1)
-  .p_info = &g_transfer1_info,
+		.p_info = &g_transfer1_info,
 #elif (1 > 1)
     .p_info              = g_transfer1_info,
 #else
     .p_info = NULL,
 #endif
-  .p_extend = &g_transfer1_cfg_extend, };
+		.p_extend = &g_transfer1_cfg_extend, };
 
 /* Instance structure to use this module. */
-const transfer_instance_t g_transfer1 =
-{ .p_ctrl = &g_transfer1_ctrl, .p_cfg = &g_transfer1_cfg, .p_api = &g_transfer_on_dtc };
+const transfer_instance_t g_transfer1 = { .p_ctrl = &g_transfer1_ctrl, .p_cfg =
+		&g_transfer1_cfg, .p_api = &g_transfer_on_dtc };
 dtc_instance_ctrl_t g_transfer0_ctrl;
 
 #if (1 == 1)
 transfer_info_t g_transfer0_info DTC_TRANSFER_INFO_ALIGNMENT =
-{ .transfer_settings_word_b.dest_addr_mode = TRANSFER_ADDR_MODE_FIXED,
-  .transfer_settings_word_b.repeat_area = TRANSFER_REPEAT_AREA_SOURCE,
-  .transfer_settings_word_b.irq = TRANSFER_IRQ_END,
-  .transfer_settings_word_b.chain_mode = TRANSFER_CHAIN_MODE_DISABLED,
-  .transfer_settings_word_b.src_addr_mode = TRANSFER_ADDR_MODE_INCREMENTED,
-  .transfer_settings_word_b.size = TRANSFER_SIZE_1_BYTE,
-  .transfer_settings_word_b.mode = TRANSFER_MODE_NORMAL,
-  .p_dest = (void*) NULL,
-  .p_src = (void const*) NULL,
-  .num_blocks = (uint16_t) 0,
-  .length = (uint16_t) 0, };
+		{ .transfer_settings_word_b.dest_addr_mode = TRANSFER_ADDR_MODE_FIXED,
+				.transfer_settings_word_b.repeat_area =
+						TRANSFER_REPEAT_AREA_SOURCE,
+				.transfer_settings_word_b.irq = TRANSFER_IRQ_END,
+				.transfer_settings_word_b.chain_mode =
+						TRANSFER_CHAIN_MODE_DISABLED,
+				.transfer_settings_word_b.src_addr_mode =
+						TRANSFER_ADDR_MODE_INCREMENTED,
+				.transfer_settings_word_b.size = TRANSFER_SIZE_1_BYTE,
+				.transfer_settings_word_b.mode = TRANSFER_MODE_NORMAL, .p_dest =
+						(void*) NULL, .p_src = (void const*) NULL, .num_blocks =
+						(uint16_t) 0, .length = (uint16_t) 0, };
 
 #elif (1 > 1)
 /* User is responsible to initialize the array. */
@@ -63,57 +65,53 @@ transfer_info_t g_transfer0_info[1] DTC_TRANSFER_INFO_ALIGNMENT;
 /* User must call api::reconfigure before enable DTC transfer. */
 #endif
 
-const dtc_extended_cfg_t g_transfer0_cfg_extend =
-{ .activation_source = VECTOR_NUMBER_SCI0_TXI, };
+const dtc_extended_cfg_t g_transfer0_cfg_extend = { .activation_source =
+		VECTOR_NUMBER_SCI0_TXI, };
 
-const transfer_cfg_t g_transfer0_cfg =
-{
+const transfer_cfg_t g_transfer0_cfg = {
 #if (1 == 1)
-  .p_info = &g_transfer0_info,
+		.p_info = &g_transfer0_info,
 #elif (1 > 1)
     .p_info              = g_transfer0_info,
 #else
     .p_info = NULL,
 #endif
-  .p_extend = &g_transfer0_cfg_extend, };
+		.p_extend = &g_transfer0_cfg_extend, };
 
 /* Instance structure to use this module. */
-const transfer_instance_t g_transfer0 =
-{ .p_ctrl = &g_transfer0_ctrl, .p_cfg = &g_transfer0_cfg, .p_api = &g_transfer_on_dtc };
+const transfer_instance_t g_transfer0 = { .p_ctrl = &g_transfer0_ctrl, .p_cfg =
+		&g_transfer0_cfg, .p_api = &g_transfer_on_dtc };
 sci_spi_instance_ctrl_t g_spi0_ctrl;
 
 /** SPI extended configuration */
-const sci_spi_extended_cfg_t g_spi0_cfg_extend =
-{ .clk_div =
-{
-/* Actual calculated bitrate: 6250000. */.cks = 0,
-  .brr = 3, .mddr = 0, } };
+const sci_spi_extended_cfg_t g_spi0_cfg_extend = { .clk_div = {
+/* Actual calculated bitrate: 6250000. */.cks = 0, .brr = 3, .mddr = 0, } };
 
-const spi_cfg_t g_spi0_cfg =
-{ .channel = 0, .operating_mode = SPI_MODE_MASTER, .clk_phase = SPI_CLK_PHASE_EDGE_ODD, .clk_polarity =
-          SPI_CLK_POLARITY_LOW,
-  .mode_fault = SPI_MODE_FAULT_ERROR_DISABLE, .bit_order = SPI_BIT_ORDER_MSB_FIRST,
+const spi_cfg_t g_spi0_cfg = { .channel = 0, .operating_mode = SPI_MODE_MASTER,
+		.clk_phase = SPI_CLK_PHASE_EDGE_ODD, .clk_polarity =
+				SPI_CLK_POLARITY_LOW,
+		.mode_fault = SPI_MODE_FAULT_ERROR_DISABLE, .bit_order =
+				SPI_BIT_ORDER_MSB_FIRST,
 #define RA_NOT_DEFINED (1)
 #if (RA_NOT_DEFINED == g_transfer0)
     .p_transfer_tx   = NULL,
 #else
-  .p_transfer_tx = &g_transfer0,
+		.p_transfer_tx = &g_transfer0,
 #endif
 #if (RA_NOT_DEFINED == g_transfer1)
     .p_transfer_rx   = NULL,
 #else
-  .p_transfer_rx = &g_transfer1,
+		.p_transfer_rx = &g_transfer1,
 #endif
 #undef RA_NOT_DEFINED
-  .p_callback = sci_spi_callback,
-  .p_context = NULL, .rxi_irq = VECTOR_NUMBER_SCI0_RXI, .txi_irq = VECTOR_NUMBER_SCI0_TXI, .tei_irq =
-          VECTOR_NUMBER_SCI0_TEI,
-  .eri_irq = VECTOR_NUMBER_SCI0_ERI, .rxi_ipl = (12), .txi_ipl = (12), .tei_ipl = (12), .eri_ipl = (12), .p_extend =
-          &g_spi0_cfg_extend, };
+		.p_callback = sci_spi_callback, .p_context = NULL, .rxi_irq =
+				VECTOR_NUMBER_SCI0_RXI, .txi_irq = VECTOR_NUMBER_SCI0_TXI,
+		.tei_irq = VECTOR_NUMBER_SCI0_TEI, .eri_irq = VECTOR_NUMBER_SCI0_ERI,
+		.rxi_ipl = (12), .txi_ipl = (12), .tei_ipl = (12), .eri_ipl = (12),
+		.p_extend = &g_spi0_cfg_extend, };
 /* Instance structure to use this module. */
-const spi_instance_t g_spi0 =
-{ .p_ctrl = &g_spi0_ctrl, .p_cfg = &g_spi0_cfg, .p_api = &g_spi_on_sci };
-void g_hal_init(void)
-{
-    g_common_init ();
+const spi_instance_t g_spi0 = { .p_ctrl = &g_spi0_ctrl, .p_cfg = &g_spi0_cfg,
+		.p_api = &g_spi_on_sci };
+void g_hal_init(void) {
+	g_common_init();
 }
