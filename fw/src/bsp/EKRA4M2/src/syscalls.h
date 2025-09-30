fd63c66d7b27aaa14cfff2c102c46d87d8849e86
Sebastiano Costanzo <sebastiano.costanzo@danfoss.com>
2025-09-30 16:48:55 +0200

    update e2studio

---
+++ fw/src/bsp/EKRA4M2/src/syscalls.h
@@ -0,0 +1,45 @@
+#ifndef __SYSCALLS
+#define __SYSCALLS
+
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/times.h>
+
+int _getpid(void);
+
+int _kill(int pid, int sig);
+
+void _exit(int status);
+
+int _read(int file, char *ptr, int len);
+
+int _write(int file, char *ptr, int len);
+
+int _close(int file);
+
+int _fstat(int file, struct stat *st);
+
+int _isatty(int file);
+
+int _lseek(int file, int ptr, int dir);
+
+int _open(char *path, int flags, ...);
+
+int _wait(int *status);
+
+int _unlink(char *name);
+
+int _times(struct tms *buf);
+
+int _stat(char *file, struct stat *st);
+
+int _link(char *old, char *new);
+
+int _fork(void);
+
+int _execve(char *name, char **argv, char **env);
+
+int _gettimeofday(struct timeval *tv, void *tzvp);
+
+#endif
